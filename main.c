c62636a4db58e273014352fb175d16a672575c07
ultrasau6 <choyatsu@gmail.com>
2026-06-18 23:33:53 +0200

    patched bugs

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
-//#include <cstdint>
 #include <math.h>
 #include <stdio.h>
 #include <fcntl.h>
 
 #include <stdlib.h>
-#include <unistd.h>    // close(), read()
-#include <string.h>    // strerror()
+#include <unistd.h>
+#include <string.h>
 
 #include <libevdev-1.0/libevdev/libevdev.h>
 #include <libevdev-1.0/libevdev/libevdev-uinput.h>
@@ -18,7 +17,9 @@ char* SelectedDev ;
 
 guint timeout_id = 0 ;
 
-int mainInterval = 10;
+int mainInterval = 1;
+
+struct libevdev *dev = NULL;
 
 int min(int a , int b){
     if (a > b ){
@@ -34,110 +35,118 @@ int max(int a , int b){
     return b;
 }
 
-void readRC(int rc, struct libevdev *dev){
-    struct input_event ev;
-    //struct libevdev *dev = NULL;
-
-    while (1) {
-        int rc = libevdev_next_event(dev,
-                                    LIBEVDEV_READ_FLAG_NORMAL | LIBEVDEV_READ_FLAG_BLOCKING,
-                                    &ev);
-
-        if (rc == LIBEVDEV_READ_STATUS_SUCCESS) {
-            // Ignorer les events de sync (EV_SYN), ils sont juste des séparateurs
-            if (ev.type == EV_ABS) {
-                printf("Axe: code=%d  valeur=%d\n", ev.code, ev.value);
-            }
-            if (ev.type == EV_KEY) {
-                printf("Bouton: code=%d  %s\n", ev.code,
-                    ev.value ? "appuyé" : "relâché");
-            }
-        }
-    }
-}
-
-
-struct input_event RumbleEffect(struct libevdev *dev){
-    //struct libevdev *dev = NULL;
-    
-    //FF_RUMBLE Vibration simple"clac" quand tu enclenchs
-    //FF_CONSTANT Force constante dans une directionRésistance sur un axe
-    //FF_SPRING Rappel vers le centreLevier qui revient en neutre
+struct input_event RumbleEffect(){
 
     int fd = libevdev_get_fd(dev);
     struct ff_effect effect;
     memset(&effect, 0, sizeof(effect));
 
     effect.type = FF_RUMBLE;
-    effect.id   = -1;  // -1 = demande au driver d'assigner un id libre
+    effect.id   = -1;
 
-    effect.u.rumble.strong_magnitude = 0xffff; // intensité moteur fort (0 à 0xffff)
-    effect.u.rumble.weak_magnitude   = 0x0000; // intensité moteur faible
+    effect.u.rumble.strong_magnitude = 0xffff;
+    effect.u.rumble.weak_magnitude   = 0x0000;
 
-    effect.replay.length = 150/2;  // durée en ms
-    effect.replay.delay  = 0;    // délai avant déclenchement
+    effect.replay.length = 150/2;
+    effect.replay.delay  = 0;
     struct input_event play;
+    
     if (ioctl(fd, EVIOCSFF, &effect) < 0) {
-        perror("upload FF");
+        perror("upload FF RumbleEffect");
         return play;
     }
 
     
     memset(&play, 0, sizeof(play));
     play.type  = EV_FF;
-    play.code  = effect.id;  // l'id assigné par le driver
-    play.value = 1;          // 1 = jouer, 0 = stopper
+    play.code  = effect.id;
+    play.value = 1;//play the effect
 
     
     return play;
 }
 
-static int ff_initialized = 0;
 struct ff_effect move;
 struct ff_effect move2;
-
 struct libevdev_uinput *uidev = NULL;
+struct input_event ev;
+struct input_event rumble ;
+
+int ff_initialized = 0;
+int Xaxe = 0 ;
+int Yaxe = 1 ;
+
+int Xpos = 0 ;
+int Ypos = 0 ;
+
+int LXpos = 0 ;
+int LYpos = 0 ;
+
+int RXpos = 0 ;
+int RYpos = 1 ;
+
+int Size = 1024;
+int borderSize = 50;
+int SizeSpeed ;
+
+int Speed = -1;
+int LSpeed = -1;
+
+int DeltaX = 0;
+int DeltaY = 0;
 
 void ChangeSettingMove(int fd){
     if (!ff_initialized) return;
 
-    if (ioctl(fd, EVIOCSFF, &move) < 0) {
-        perror("init FF constant");
-        return;
-    }
+    if (ioctl(fd, EVIOCSFF, &move) < 0) perror("init FF constant move"); return;
 }
 
-void ConstMove(struct libevdev *dev){
-    //struct libevdev *dev = NULL;
-    
-    //FF_RUMBLE Vibration simple"clac" quand tu enclenchs
-    //FF_CONSTANT Force constante dans une directionRésistance sur un axe
-    //FF_SPRING Rappel vers le centreLevier qui revient en neutre
+int ConstMove(){
 
     int fd = libevdev_get_fd(dev);
     
+    printf("%s\n",libevdev_get_name(dev));
+    
+    if (!libevdev_has_event_code(dev, EV_FF, FF_CONSTANT)) {
+        perror("FF_CONSTANT non supporté\n");
+        return -1;
+    }
+
+    // Vérifier le support FF_RUMBLE
+    if (!libevdev_has_event_code(dev, EV_FF, FF_RUMBLE)) {
+        perror("FF_RUMBLE non supporté\n");
+        return -1;
+    }
+
+    if (fd < 0){
+        perror("fd<0\n");
+        return -1;
+    }
+    else{
+        printf("fd ok\n");
+    }
+
     memset(&move, 0, sizeof(move));
     
     move.type = FF_CONSTANT;
-    move.id = -1;  // -1 = demande au driver d'assigner un id libre
-    move.replay.length        = 0xFFFF;   // durée "infinie"
+    move.id = -1;
+    move.replay.length        = 0xFFFF;
     move.replay.delay         = 0;
     move.u.constant.level     = 0x7FFF;
     move.direction            = 0xC000;
 
-    move.u.constant.envelope.attack_length  = 0;
-    move.u.constant.envelope.attack_level   = 0;
-    move.u.constant.envelope.fade_length    = 50;  // 50ms de relâche
-    move.u.constant.envelope.fade_level     = 0;
+    move.u.constant.envelope.attack_length  =  0 ;
+    move.u.constant.envelope.attack_level   =  0 ;
+    move.u.constant.envelope.fade_length    = 50 ;
+    move.u.constant.envelope.fade_level     =  0 ;
 
     struct input_event play;
 
     if (ioctl(fd, EVIOCSFF, &move) < 0) {
-        perror("init FF constant");
-        return;
+        perror("init FF constant const");
+        return -1;
     }
 
-    // Démarrer l'effet (il reste actif, on met juste à jour son level)
     memset(&play, 0, sizeof(play));
     play.type  = EV_FF;
     play.code  = move.id;
@@ -145,8 +154,8 @@ void ConstMove(struct libevdev *dev){
     write(fd, &play, sizeof(play));
 
     ff_initialized = 1;
+    return 0;
 }
-
 
 
 void XCenterForce(int fd,int Xpos,int DeltaX){
@@ -170,124 +179,100 @@ void XCenterForce(int fd,int Xpos,int DeltaX){
     ChangeSettingMove(fd);
 }
 
-void readRControl(int rc, struct libevdev *dev){
-    struct input_event ev;
-    struct input_event rumble = RumbleEffect(dev);
+int init_dev(){
+    printf("init\n");
+    ConstMove();
+
+    rumble = RumbleEffect();
+
     int fd = libevdev_get_fd(dev);
-    ConstMove(dev);
+    if (fd < 0) return -1;
+
+    if (rumble.code >= 0) write(fd, &rumble, sizeof(rumble));
+    
     write(fd, &move, sizeof(move));
 
-    int Xaxe = 0 ;
-    int Yaxe = 1 ;
+    SizeSpeed = ((1024/3)-borderSize);
 
-    int Xpos = 0 ;
-    int Ypos = 0 ;
-
-    int LXpos = 0 ;
-    int LYpos = 0 ;
-
-    int RXpos = 0 ;
-    int RYpos = 1 ;
-
-    int Size = 1024;
-    int borderSize = 50;
-    int SizeSpeed = (1024/3)-borderSize;
-    
-    int Speed = -1;
-    int LSpeed = -1;
-
-    int DeltaX = 0;
-    int DeltaY = 0;
-    //wait until ready
-    while (!ff_initialized){};
-
-    
-
-    while (1) {
-        int rc = libevdev_next_event(dev,
-                                    LIBEVDEV_READ_FLAG_NORMAL | LIBEVDEV_READ_FLAG_BLOCKING,
-                                    &ev);
-
-        if (rc == LIBEVDEV_READ_STATUS_SUCCESS) {
-            if (ev.type == EV_ABS) {
-
-                DeltaX = Xpos - LXpos;
-                DeltaY = Ypos - LYpos;
-
-                if (ev.code == Xaxe){
-                    Xpos = ev.value;
-                    
-                }
-                else if (ev.code == Yaxe){
-                    Ypos = ev.value;
-                }
-
-                
-
-
-
-                if (abs(Ypos) > 350 ){
-                     move.u.constant.level = 32767/2;
-                    if ((Ypos)>0){
-                        move.direction = 0x0000;
-                        ChangeSettingMove(fd);
-                    }
-                    else {
-                        move.direction = 0x8000;
-                        ChangeSettingMove(fd);
-                    }
-
-
-
-                    RXpos = Xpos+512;RYpos = Ypos+512;
-                    if (Speed == -1){
-                        if (Ypos>0){
-                            Speed = floor(RXpos/(1024/3));
-                        }
-                        else{
-                            Speed = floor(RXpos/(1024/3)+3);
-                        }
-                    }
-                
-                    
-                }
-                else {
-
-
-
-                    Speed = -1;
-                    //int force = 10000 ;
-
-                    
-
-                    XCenterForce(fd,Xpos,DeltaX);
-                    //YCenterForce(fd,Ypos,DeltaY);
-                }
-
-
-
-
-
-                if (Speed!=LSpeed){
-                        write(fd, &rumble, sizeof(rumble));
-                        printf("speed : %d %d\n",Speed,LSpeed);
-                        libevdev_uinput_write_event(uidev, EV_KEY, BTN_TRIGGER_HAPPY2 +LSpeed, 0);
-                        libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
-                        
-                        libevdev_uinput_write_event(uidev, EV_KEY, BTN_TRIGGER_HAPPY2 +Speed, 1); // appui
-                        libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
-                }
-
-                LXpos = Xpos;
-                LYpos = Ypos;
-                LSpeed = Speed;
-                //printf("x=%d  y=%d\n", Xpos, Ypos);
-            }
-        }
-    }
+    printf("init end\n");
+    return 0;
 }
 
+gboolean gearboxFF(gpointer data){
+    int fd = libevdev_get_fd(dev);
+    if (fd < 0 ){
+        printf("fd<0\n");
+        return G_SOURCE_CONTINUE;
+    }
 
+    while (!ff_initialized){};
+
+    int rc = libevdev_next_event(dev,
+                                LIBEVDEV_READ_FLAG_NORMAL | LIBEVDEV_READ_FLAG_BLOCKING,
+                                &ev);
+    
+    
+    if (rc == LIBEVDEV_READ_STATUS_SUCCESS) {
+        if (ev.type == EV_ABS) {
+
+            DeltaX = Xpos - LXpos;
+            DeltaY = Ypos - LYpos;
+
+            if (ev.code == Xaxe){
+                Xpos = ev.value;
+                
+            }
+            else if (ev.code == Yaxe){
+                Ypos = ev.value;
+            }
+
+            
+
+
+
+            if (abs(Ypos) > 350 ){
+                    move.u.constant.level = 32767/2;
+                if ((Ypos)>0){
+                    move.direction = 0x0000;
+                    ChangeSettingMove(fd);
+                }
+                else {
+                    move.direction = 0x8000;
+                    ChangeSettingMove(fd);
+                }
+
+
+
+                RXpos = Xpos+512;RYpos = Ypos+512;
+                if (Speed == -1){
+                    if (Ypos>0){
+                        Speed = floor(RXpos/(1024/3));
+                    }
+                    else{
+                        Speed = floor(RXpos/(1024/3)+3);
+                    }
+                }
+            }
+            else 
+            {
+                Speed = -1;
+                //int force = 10000 ;
+                XCenterForce(fd,Xpos,DeltaX);
+            }
+
+            if (Speed!=LSpeed){
+                    write(fd, &rumble, sizeof(rumble));
+                    printf("speed : %d %d\n",Speed,LSpeed);
+            }
+
+            LXpos = Xpos;
+            LYpos = Ypos;
+            LSpeed = Speed;
+        }
+    }
+    
+    return G_SOURCE_CONTINUE;
+}
 
 int getfdFromPath(const char* path){
     int fd = open(path, O_RDWR | O_NONBLOCK);
@@ -308,8 +293,9 @@ int getrcFromfd(const int fd , struct libevdev **dev){
 }
 
 char** GetDevicesNames(char* names[],int *n){
-    struct libevdev *dev = NULL;
+    struct libevdev *devFind = NULL;
     char* path = malloc(250);
+    int peripheralFF_Number = 0;
     int peripheral_Number = 0;
     
     while (1){
@@ -318,14 +304,27 @@ char** GetDevicesNames(char* names[],int *n){
             int fd = getfdFromPath(path);
             if (fd == -1){free(path);printf("fd -1\n");return 0;}
 
-            int rc = getrcFromfd(fd,&dev);
+            int rc = getrcFromfd(fd,&devFind);
             if (rc == -1){free(path);printf("rc -1\n");return 0;}
-            names[peripheral_Number] = libevdev_get_name(dev);
+
+            
+            printf("FF ok\n");
+            if (libevdev_has_event_code(devFind, EV_FF, FF_CONSTANT)&&
+                libevdev_has_event_code(devFind, EV_FF, FF_RUMBLE))
+            {
+                printf("FF ok %s \n",libevdev_get_name(devFind));
+                names[peripheralFF_Number] = libevdev_get_name(devFind);
+                peripheralFF_Number += 1;
+            }
+            else{
+                printf("FF no ok\n");
+            }
             peripheral_Number++;
+
         }
         else{break;}
     }
-    *n = peripheral_Number;
+    *n = peripheralFF_Number;
     return names;
 }
 
@@ -342,24 +341,23 @@ int FindDeviceByName(char* name, struct libevdev **dev){
         printf("%s : ",path);
         if (access(path, F_OK) == 0) {
             // if file exists
-            int fd = getfdFromPath(path);
-            if (fd == -1){free(path);printf("fd -1\n");return -1;}
+            int fdF = getfdFromPath(path);
+            if (fdF == -1){free(path);printf("fdF -1\n");return -1;}
 
-            int rc = getrcFromfd(fd,dev);
-            if (rc == -1){free(path);printf("rc -1\n");return -1;}
+            int rcF = getrcFromfd(fdF,dev);
+            if (rcF == -1){free(path);printf("rcF -1\n");return -1;}
 
             printf("%s\n",libevdev_get_name(*dev));
 
             if (strcmp(libevdev_get_name(*dev),name) == 0){
                 printf("joystick found\n");
-                close(fd);
                 free(path);
-                return rc;
+                return rcF;
             }
             // if the peripheral is not the one we search, we remove it and set the counter to ++ to setup the next one
             libevdev_free(*dev);
             //*dev = NULL;
-            close(fd);
+            close(fdF);
             perNum++;
 
         } else {
@@ -375,8 +373,13 @@ int FindDeviceByName(char* name, struct libevdev **dev){
     return -1;
 }
 
+int rcMain;
+
 void ChangePeripheral(GtkWidget *combo){
     printf("\n%s\n",gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo)));
+    SelectedDev = gtk_combo_box_text_get_active_text(GTK_COMBO_BOX_TEXT(combo));
+    rcMain =   FindDeviceByName(SelectedDev,&dev);
+    printf("%d\n",rcMain);
 }
 
 void RefreshPeripheral(GtkWidget *button,gpointer data){
@@ -415,8 +418,8 @@ void activate(GtkWidget *button,gpointer data){
     else{
 
         gtk_window_set_title(GTK_WINDOW(window), "JoytoGear - activated");
-
-        timeout_id = g_timeout_add(mainInterval,  placholder, NULL);
+        init_dev();
+        timeout_id = g_timeout_add(mainInterval,  gearboxFF, NULL);
         activated=1;
     }
     
@@ -467,6 +470,8 @@ GtkWidget* init_gtk(int argc, char *argv[]){
     g_signal_connect(btnActivate, "toggled", G_CALLBACK(activate), window);
 
     gtk_container_add(GTK_CONTAINER(window), boxWindow);
+
+    RefreshPeripheral(NULL,combo);
     return window;
 }
 
@@ -476,12 +481,11 @@ int main(int argc, char *argv[]) {
 
     GtkWidget *window = init_gtk(argc,argv);
 
-    struct libevdev *dev = NULL;
+    rcMain =   FindDeviceByName(SelectedDev,&dev);
 
-
-
-    int rc = FindDeviceByName("Microsoft SideWinder Force Feedback 2 Joystick",&dev);
-    if (rc < 0){return -1;}
+    if (rc < 0) return -1;
+    int fd = libevdev_get_fd(dev);
+    if (fd < 0) return -1;
 
     struct libevdev *vdev = libevdev_new();
     libevdev_set_name(vdev, "virtual-shifter");
@@ -494,11 +498,11 @@ int main(int argc, char *argv[]) {
     
     int rc2 = libevdev_uinput_create_from_device(vdev,LIBEVDEV_UINPUT_OPEN_MANAGED,&uidev);
     if (rc2 < 0) {
-    //    fprintf(stderr, "Erreur uinput: %s\n", strerror(-rc));
+        fprintf(stderr, "Erreur uinput: %s\n", strerror(-rc));
         return 1;
     }
     libevdev_free(vdev);
-    //readRControl(rc,dev);
+
     gtk_widget_show_all(window);
     gtk_main();
     return 0;
